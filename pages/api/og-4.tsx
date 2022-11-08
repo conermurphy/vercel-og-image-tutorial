1ef8d8bea6bb2b6b165c9db10564902ac2d94c97
Coner Murphy <coner@conermurphy.com>
2022-11-08 18:51:51 +0000

    fix: fixed build errors

--- pages/api/og-4.tsx
+++ pages/api/og-4.tsx
@@ -69,7 +69,6 @@ export default function handler(req: NextRequest) {
       }
     );
   } catch (e) {
-    console.log(`${e.message}`);
     return new Response(`Failed to generate the image`, {
       status: 500,
     });
